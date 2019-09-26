815159d58e63c48e8ee888803d09216c5384f082
Mateus Santana Gomes Nascimento <msgn2@WINDOWS.CIN.UFPE.BR>
2019-09-26 16:27:21 -0300

    Codigos aula 10

--- aula10.hs
+++ aula10.hs
@@ -2,3 +2,26 @@
 -- getChar :: IO Char
 -- putStr :: String -> IO ()
 -- putStrLn :: String -> IO ()
+
+myputStrLn :: String -> IO ()
+myputStrLn str = 
+    do
+        putStr str
+        putStr "\n"
+        putStr "\n"
+
+putNtimes :: Int ->String -> IO ()
+putNtimes 0 str = return ()
+putNtimes n str =
+    do
+        putStrLn str
+        putNtimes (n-1) str
+
+somaES :: IO ()
+somaES =
+    do
+        putStrLn "Entre o primeiro numero"
+        xs <- getLine
+        putStrLn "Entre o segundo numero"
+        ys <- getLine
+        putStrLn ("O resultado é: " ++ show((read xs) + (read ys)))
